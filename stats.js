8eb931743a006ad3db05caaf3412fc65e45b4292
jittervpn <diegogz143@gmail.com>
2026-08-21 22:21:22 -0300

    Delete stats.js

--- stats.js
+++
@@ -1,8 +0,0 @@
-import { json, cleanupIfDue } from "../_lib.js";
-
-export async function onRequestGet({ env }) {
-  await cleanupIfDue(env);
-  const total = parseInt((await env.JITTERX_KV.get("stats:total")) || "0", 10);
-  const active = parseInt((await env.JITTERX_KV.get("stats:active")) || "0", 10);
-  return json({ total, active, max: parseInt(env.MAX_USERS || "100", 10), zone: env.ZONE_NAME });
-}
